3fb508298ef85c29a45eee30292cd00c10add937
Samuel Gonzalez <samglz10@gmail.com>
2023-01-20 19:28:17 -0500

    first push

---
+++ index.js
@@ -0,0 +1,17 @@
+//https://expressjs.com/en/guide/routing.html
+
+//Require needed modules
+const express = require('express') 
+//initializes the app object
+const app = express() 
+
+// forward slash leads back to root or home
+//req or request listens for routes
+app.get('/',(req,res) => {
+    res.send('hello homie')
+    console.log(res)
+})
+
+app.listen(3000, ()=> {
+    console.log('listening on http://localhost:3000/')
+})
